991422b18b3a10675ec495c18f8161fe1190058c
Skye <me@skye.vg>
2023-04-16 23:01:34 +0900

    chore: minify more stuff

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
+import { createHtmlPlugin } from 'vite-plugin-html'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 /** @type {import('vite').UserConfig} */
 export default {
-
+    plugins: [
+        createHtmlPlugin({
+            minify: true,
+        }),
+        ViteImageOptimizer()
+    ],
+    build: {
+        modulePreload: {
+            polyfill: false
+        }
+    }
 }
